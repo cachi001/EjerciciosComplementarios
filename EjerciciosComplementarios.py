e40549b860774a57bfca2fa3d22dcf1bf78bf46e
cachi001 <140206947+cachi001@users.noreply.github.com>
2023-09-28 10:51:04 -0300

    Add files via upload
    Emiliano Cáceres

---
+++ EjerciciosComplementarios.py
@@ -0,0 +1,111 @@
+#Ejercicio 1 y 2
+numero1 = 5
+numero2 = 2.5
+
+print ("Numero 1: ",numero1)
+print ("Numero 2: ",numero2)
+
+#Ejercicio 3 y 4
+print("---------------------------------------------------------------------")
+suma = numero1 + numero2
+
+print ("\nLa suma de los dos numeros es: ",suma)
+
+resta = numero1 - numero2
+
+print ("\nLa resta de los dos numeros es: ",resta)
+
+multiplicacion = numero1 * numero2
+
+print ("\nLa multiplicacion de los dos numeros es: ", multiplicacion)
+
+division = numero1 / numero2
+
+print ("\nLa division de los dos numeros es: ", division)
+
+#Ejercicio 5
+print("---------------------------------------------------------------------")
+nombre1 = "Emiliano"
+print ("\nMi nombre es ", nombre1)
+
+#Ejercicio 6
+print("---------------------------------------------------------------------")
+precio = 750
+print ("\nEl Precio de la Coca Cola es $",precio)
+
+#Ejercicio 7
+descuento = 0.50
+print ("\nEl descuento que tiene es de un ", descuento*100,"%")
+
+#Ejercicio 8
+precio_final = (precio * descuento) + precio;
+print ("\nEl precio final de la Coca Cola es de $", descuento*100,"%")
+
+#Ejercicio 9 y 10
+print("---------------------------------------------------------------------")
+cadena = "Ejercicios"
+longitud = len(cadena)
+
+print ("\nLa cadena es ", cadena," y su longitud de ",longitud)
+
+#Ejercicio 11
+print("---------------------------------------------------------------------")
+precio_float = 45.5
+
+print ("\nPrecio sin convertir ", precio_float)
+print ("\nPrecio Convertido ", int (precio_float))
+
+#Ejercicio 12
+print("---------------------------------------------------------------------")
+nombre = "Emiliano "
+apellido = "Cáceres"
+
+print ("\nNombre: ", nombre)
+print ("\nApellido: ", apellido)
+
+nombre_completo = nombre + apellido
+
+print ("\nNombre Completo: ", nombre_completo)
+
+#Ejercicio 13
+print("---------------------------------------------------------------------")
+edad = 19
+print ("\nEdad: ", edad)
+
+edad += 5
+print ("\nEdad incrementada en 5: ", edad)
+
+edad -=10
+print ("\nEdad disminuida en 10: ", edad)
+
+#Ejercicio 14
+print("---------------------------------------------------------------------")
+altura = 1.78
+print ("\nAltura: ", altura,"m")
+
+altura *= 4
+print ("\nAltura multiplicada por 4", altura,"m")
+
+altura /= 3
+print ("\nAltura dividida por 3", altura,"m")
+
+#Ejercicio 15
+print("---------------------------------------------------------------------")
+nombre_mayus = "EMILIANO"
+print ("\nNombre mayusculas", nombre_mayus)
+
+nombre_minus = nombre_mayus.lower()
+print ("\nNombre minusculas", nombre_minus)
+
+#Ejercicio 16
+print("---------------------------------------------------------------------")
+nombre_mayusminus = nombre.capitalize()
+print ("\nNombre con la primera letra en mayusculas y lo demas en minusculas", nombre_mayusminus)
+print("---------------------------------------------------------------------")
+
+
+
+
+
+
+
